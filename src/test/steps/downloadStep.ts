0734d7d8d06039b27e1b273a2dd8a78560f47b90
DarshanRaj-2005 <dharshujayapal@gmail.com>
2026-08-12 22:06:09 +0530

    changed downloadstep file to make it little bit effective

--- src/test/steps/downloadStep.ts
+++ src/test/steps/downloadStep.ts
@@ -2,14 +2,21 @@ import { expect } from "@playwright/test";
 import { When, Then } from "@cucumber/cucumber";
 import { customWorld } from "../world/customWorrld";
 
-When("the user click on the Export to Excel button", async function (this: customWorld) {
-    this.download = await this.homePage.clickDownloadButton()
-});
+When(
+  "the user click on the Export to Excel button",
+  async function (this: customWorld) {
+    this.download = await this.homePage.clickDownloadButton();
+  }
+);
 
-Then("the Excel file should be downloaded successfully", async function (this:customWorld) {
+Then(
+  "the Excel file should be downloaded successfully",
+  async function (this: customWorld) {
+    expect(this.download).toBeDefined();
     const failure = await this.download.failure();
     expect(failure).toBeNull();
     const fileName = this.download.suggestedFilename();
+    expect(fileName).toMatch(/\.xlsx$/);
     await this.download.saveAs(`downloads/${fileName}`);
-    expect(this.homePage.downloadBtn).toBeTruthy();
-});
+  }
+);
